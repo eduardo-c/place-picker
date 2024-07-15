4ab426614efc8f52c469ad87894335ea04cfc009
Castro Castro <jcastrocastro@deloitte.com>
2024-07-15 15:57:30 -0600

    Completing the fetching available places used by custom hook to include the sorting feature

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -4,25 +4,31 @@ import { sortPlacesByDistance } from '../loc.js';
 import { fetchAvailablePlaces } from '../http.js';
 import { useFetch } from '../hooks/useFetch.js';
 
-/*navigator.geolocation.getCurrentPosition((position) => {
-  const sortedPlaces = sortPlacesByDistance(
-    places,
-    position.coords.latitude,
-    position.coords.longitude
-  );
-  setAvailablePlaces(sortedPlaces);
-  setIsFetching(false);
-});*/
+async function getSortedPlaces() {
+  const places = await fetchAvailablePlaces();
+
+  // Using the standard Promise pattern from JS to convert the returned object as a promise
+  return new Promise((resolve) => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const sortedPlaces = sortPlacesByDistance(
+        places,
+        position.coords.latitude,
+        position.coords.longitude
+      );
+
+      resolve(sortedPlaces);
+    });
+  });
+}
 
 export default function AvailablePlaces({ onSelectPlace }) {
 
   // Now we make use of useFetch custom hook from another component
   // Note that the state values declared in the custom hook are different for every importing component
   // That is, every component has it's own state value snapshot and state change in one, doesn't impact state in other importing component
-  const {fetchedData: availablePlaces,
+  const { fetchedData: availablePlaces,
     isFetching,
-    error,
-    setData: setAvailablePlaces} = useFetch(fetchAvailablePlaces, []);
+    error } = useFetch(getSortedPlaces, []);
 
   if (error) {
     return <Error title="An error occurred!" message={error.message} />;
