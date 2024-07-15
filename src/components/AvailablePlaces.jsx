979df65401f5a76a6774c8c0e35071fdd5597588
Castro Castro <jcastrocastro@deloitte.com>
2024-07-15 15:34:46 -0600

    Using the useFetch custom hook in other component (AvailablePlaces.jsx)

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,42 +1,28 @@
-import { useState, useEffect } from 'react';
-
 import Places from './Places.jsx';
 import Error from './Error.jsx';
 import { sortPlacesByDistance } from '../loc.js';
 import { fetchAvailablePlaces } from '../http.js';
+import { useFetch } from '../hooks/useFetch.js';
+
+/*navigator.geolocation.getCurrentPosition((position) => {
+  const sortedPlaces = sortPlacesByDistance(
+    places,
+    position.coords.latitude,
+    position.coords.longitude
+  );
+  setAvailablePlaces(sortedPlaces);
+  setIsFetching(false);
+});*/
 
 export default function AvailablePlaces({ onSelectPlace }) {
-  const [isFetching, setIsFetching] = useState(false);
-  const [availablePlaces, setAvailablePlaces] = useState([]);
-  const [error, setError] = useState();
 
-  useEffect(() => {
-    async function fetchPlaces() {
-      setIsFetching(true);
-
-      try {
-        const places = await fetchAvailablePlaces();
-
-        navigator.geolocation.getCurrentPosition((position) => {
-          const sortedPlaces = sortPlacesByDistance(
-            places,
-            position.coords.latitude,
-            position.coords.longitude
-          );
-          setAvailablePlaces(sortedPlaces);
-          setIsFetching(false);
-        });
-      } catch (error) {
-        setError({
-          message:
-            error.message || 'Could not fetch places, please try again later.',
-        });
-        setIsFetching(false);
-      }
-    }
-
-    fetchPlaces();
-  }, []);
+  // Now we make use of useFetch custom hook from another component
+  // Note that the state values declared in the custom hook are different for every importing component
+  // That is, every component has it's own state value snapshot and state change in one, doesn't impact state in other importing component
+  const {fetchedData: availablePlaces,
+    isFetching,
+    error,
+    setData: setAvailablePlaces} = useFetch(fetchAvailablePlaces, []);
 
   if (error) {
     return <Error title="An error occurred!" message={error.message} />;
